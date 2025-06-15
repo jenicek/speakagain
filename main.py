3f2e773afadecd10e51638ab37a4e3e2e11d6bbb
Tomas Jenicek <tojenicek@gmail.com>
2025-06-15 15:31:32 +0200

    Create tts API endpoint

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
+import os
+import requests
+from supabase import create_client, Client
 from fastapi import FastAPI
+from fastapi.responses import Response
 
 app = FastAPI()
+
+
+ELEVENLABS_API_KEY = os.environ["ELEVENLABS_API_KEY"]
+SUPABASE_URL = os.environ["SUPABASE_URL"]
+SUPABASE_KEY = os.environ["SUPABASE_KEY"]
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 
 @app.get("/")
 def index():
     return {"message": "Hello World"}
+
+
+@app.post("/tts")
+def tts(text: str, user_id: str, gender: str):
+    """
+    Text-to-Speech endpoint.
+    :param text: The text to convert to speech.
+    :return: An MP3 file containing the speech.
+    """
+    response = supabase.table("user_voices").select("*").filter("user_id", "eq", user_id).execute()
+    voice_id = response.data[0]['elevenlabs_voice_id']
+    if voice_id is None:
+        if gender == "m":
+            voice_id = "tzHEU0BULePYMOTlJocD"
+        else:
+            voice_id = "Xb7hH8MSUJpSbSDYk0k2"
+
+    resp = requests.post(f"https://api.elevenlabs.io/v1/text-to-speech/{voice_id}",
+        headers={"Xi-Api-Key": ELEVENLABS_API_KEY},
+        json={
+            "text": text,
+            "model_id": "eleven_multilingual_v2",
+        }
+    )
+    raw = resp.content
+
+    return Response(content=raw, media_type="audio/mpeg",
+                    headers={"Content-Disposition": "attachment; filename=output.mp3"})
 
 
 if __name__ == "__main__":
